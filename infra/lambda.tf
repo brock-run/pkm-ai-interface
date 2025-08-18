dc13afdc681f9718753be51f9b2baff9bfcc0273
Brock <63455800+brock-run@users.noreply.github.com>
2025-08-18 19:27:34 -0400

    chore: capture linux provider checksum

---
+++ infra/lambda.tf
@@ -0,0 +1,17 @@
+resource "aws_lambda_function" "this" {
+  function_name = var.lambda_function_name
+  handler       = var.lambda_handler
+  runtime       = var.lambda_runtime
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "lambda_function_payload.zip"
+  memory_size   = 128
+  timeout       = 15
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/*"
+}
